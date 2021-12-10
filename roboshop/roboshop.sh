a69794c0fd09eabcd64a57fe604eb15dd4ebc806
mohanreddy0532 <mohanreddy0532@gmail.com>
2021-12-10 10:50:58 +0530

    comments added

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Welcome to RoboShop Project AUTOMATION(Shell Scripting).
 
+ #To check user id have root access or not.
  USER_UID=$(id -u)
  if [ ${USER_UID} -ne 0 ]; then
    echo -e "\e[1;31mYou Should be a ROOT user to run this Program\e[0m"
@@ -8,11 +9,12 @@
  fi
 
  COMPONENT=$1
+ # -z is compoent is missing
  if [ -z "$COMPONENT" ]; then
    echo -e "\e[1,31mComponent Input is Missing\e[0m"
    exit
  fi
-
+ # -e if file is exits
  if [ ! -e components/${COMPONENTS}.sh ]; then
    echo -e "\e[1;31mGiven component script does not exits\e[0m"
    exit
