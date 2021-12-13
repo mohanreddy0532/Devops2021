6c6f7bb70a2ec1ea1d5eccf83ed63955d8d2e871
mohan <mohanreddy0532@gmail.com>
2021-12-13 18:17:16 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,24 +1,3 @@
-##!/bin/bash
-#
-## source is nothing but import , like export command
-#source components/common.sh
-#
-#yum install nginx -y &>>${LOG_FILE}
-#STAT_CHECK $? "Nginx Installation"
-#
-#DOWNLOAD frontend
-#
-#rm -rf /usr/share/nginx/html/*
-#STAT_CHECK $? "Remove old HTML Pages"
-#
-#cd  /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
-#STAT_CHECK $? "Copying Frontend Content"
-#
-#cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-#STAT_CHECK $? "Update Nginx Config File"
-#
-#systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
-#STAT_CHECK $? "Restart Nginx"
 #!/bin/bash
 
 # source is nothing but import , like export command
