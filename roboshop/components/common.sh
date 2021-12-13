436380f298a960bc45984085f243d50a2b246428
mohan <mohanreddy0532@gmail.com>
2021-12-13 18:11:05 +0530

    common

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -17,7 +17,7 @@ STAT_CHECK() {
     SPACE=$(echo -n "${SPACE} ")
     LEFT=$((${LEFT}-1))
   done
-  if [ $1 -ne 0]; then
+  if [ $1 -ne 0 ]; then
     echo -e "\e[1m${2}${SPACE} - \e[1;31mFAILED\e[0m"
     exit 1
   else
