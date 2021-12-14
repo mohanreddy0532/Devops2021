da9a6bd73f96eb274194c60204cbffc16f4d957f
mohan <mohanreddy0532@gmail.com>
2021-12-14 11:27:05 +0530

    common code

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -26,7 +26,7 @@ STAT_CHECK() {
   fi
 }
 
-set-hostname ${COMPONENT}
+set-hostname -skip-apply ${COMPONENT}
 
 DOWNLOAD() {
   curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" $>>${LOG_FILE}
