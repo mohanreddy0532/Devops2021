987b25aaa4e8d714328f3347365661f5116357e7
mohan <mohanreddy0532@gmail.com>
2021-12-14 11:09:42 +0530

    frontend update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -17,5 +17,12 @@ STAT_CHECK $? "Copying Frontend Content"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $? "Update Nginx Config File"
 
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' \
+       -e '/cart/ s/localhost/cart.roboshop.internal/' \
+       -e '/user/ s/localhost/user.roboshop.internal/' \
+       -e  '/shipping/ s/localhost/shipping.roboshop.internal/' \
+       -e  '/payment/ s/localhost/payment.roboshop.internal/'  /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Update Nginx Config File"
+
 systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
 STAT_CHECK $? "Restart Nginx"
