cd6e9edddbc1b59851b68d96d05c64317e0da13f
mohan <mohanreddy0532@gmail.com>
2021-12-15 12:20:49 +0530

    redirections output> and  Input<

--- 08.redirection.sh
+++ 08.redirection.sh
@@ -7,3 +7,16 @@
 
 ls -ld abc.txt >/tmp/output 2>/tmp/err
 ls -ld abc.txt &>/tmp/both
+
+ls &>/dev/null #Null file redirection
+
+#Redirect Inputs
+To show db without login to mongo
+$echo 'show dbs;' | mongo
+#vi /tmp/mongo
+show dbs;
+show collections;
+mongo < /tmp/mongo
+
+
+
