5cb0928b33ffea3e89c9e4f207ece06226c3e824
mohan <mohanreddy0532@gmail.com>
2021-12-16 18:27:59 +0530

    redirect o/p

--- ss-robo-practice/components/frontend.sh
+++ ss-robo-practice/components/frontend.sh
@@ -18,12 +18,17 @@ STAT_CHECK  $?  "Nginx Install"
 curl -f -s -L -o /tmp/frontend.zip  "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 STAT_CHECK  $?  "Download frontend"
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip -o /tmp/frontend.zip  &>>${LOG_FILE}
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>>${LOG_FILE}
-systemctl enable nginx
-systemctl restart nginx
+rm -rf  /usr/share/nginx/html/*
+STAT_CHECK $? "Download Frontend"
+
+cd /tmp && unzip -o /tmp/frontend.zip  &>>${LOG_FILE}
+STAT_CHECK $? "Extracting FrontEnd contenet"
+
+cd /tmp/frontend-main/static && cp -r * /usr/share/nginx/html/
+STAT_CHECK $? "Copying FrontEnd content"
+
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Update Nginx Config File"
+
+systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
+STAT_CHECK $? "Restart NginX"
