7561f6262a1124be6e815ed8262434effa1a94ab
mohan <mohanreddy0532@gmail.com>
2021-12-16 12:12:21 +0530

    frontend

--- ss-robo-practice/roboshop.sh
+++ ss-robo-practice/roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER_UID=$(id -u)
-if [ ${USER_UID} -ne 0]; then
+if [ ${USER_UID} -ne 0 ]; then
   echo -e "\e[1;31mYou Should be a root user to perform this script\e[0m"
   exit
 fi
