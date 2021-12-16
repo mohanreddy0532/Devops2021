31e023cd33018e891dae7f6f93dc2a344900c3f2
mohan <mohanreddy0532@gmail.com>
2021-12-16 12:05:36 +0530

    roboshop

---
+++ ss-robo-practice/frontend.sh
@@ -0,0 +1,30 @@
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+#Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#Deploy in Nginx Default Location.
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master static README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#Finally restart the service once to effect the changes.
+
+# systemctl restart nginx
+
+echo frontend setup
+yum install nginx -y
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
