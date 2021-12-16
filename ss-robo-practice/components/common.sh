a20a9c5e930a32259e3288adf00f81c820da484d
mohan <mohanreddy0532@gmail.com>
2021-12-16 19:04:27 +0530

    redirect o/p

--- ss-robo-practice/components/common.sh
+++ ss-robo-practice/components/common.sh
@@ -18,7 +18,7 @@ DOWNLOAD() {
   curl -f -s -L -o /tmp/${1}.zip  "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
   STAT_CHECK $? "Download ${1} Code"
 
-  cd /tmp && unzip -o /tmp/frontend.zip  &>>${LOG_FILE}
+  cd /tmp && unzip -o /tmp/${1}.zip  &>>${LOG_FILE}
   STAT_CHECK $? "Extracting ${1} code"
 
 }
