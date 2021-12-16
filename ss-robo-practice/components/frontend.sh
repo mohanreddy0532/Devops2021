13fde684a128494f62a60edfabda374ebb818ab6
mohan <mohanreddy0532@gmail.com>
2021-12-16 16:33:53 +0530

    frontend

--- ss-robo-practice/components/frontend.sh
+++ ss-robo-practice/components/frontend.sh
@@ -25,7 +25,7 @@ STAT_CHECK() {
  fi
 }
 
-yum install nginx -y
+yum install nginx -y &>/dev/null
 STAT_CHECK $? "Nginx Install Failed"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
