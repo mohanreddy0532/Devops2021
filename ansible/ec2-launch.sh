7adc233ca58a9279cecdea6c3ad990b2a1233695
mohan <mohanreddy0532@gmail.com>
2021-12-17 18:24:44 +0530

    ec2 cli launch

---
+++ ansible/ec2-launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#1. Create an SPOT instance
+#2. Take that Instance IP & register in DNS
+
+TEMP_ID="lt-0371737b9b36fe546"
+TEMP_VER=1
+
+## Check if instance is already ther e
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+
+# Update the DNS record
