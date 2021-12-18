96191e316be69188aa18f03f5fa5da4f15224b26
mohan <mohanreddy0532@gmail.com>
2021-12-18 15:24:44 +0530

    ec2 instance updated

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -3,19 +3,51 @@
 #1. Create an SPOT instance
 #2. Take that Instance IP & register in DNS
 
+if [ -z "$1" ]; then
+  echo -e "\e[1;31mInput is missing\e[0m"
+  exit 1
+fi
+
+COMPONENT=$1
+
+
 TEMP_ID="lt-0371737b9b36fe546"
 TEMP_VER=1
 
-## Check if instance is already ther e
-
 #Below is with Spot-Instance and Tag
 #aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
-
 #Simple launch
 #aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
 
+## Check if instance is already there or Not
+
 #With only instance and Tag
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"| jq
+CREATE_INSTANCE() {
+##Check if Instance is already there
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+if [ $? -eq -0 ]; then
+  echo -e "\e[1;33mInstance is already there\e[0m"
+else
+ aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq
 
+#For Spot Instance
+#aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  fi
 
-# Update the DNS record
+  sleep 10
+
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+
+  # Update the DNS record
+  sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+if [ "$COMPONENT" == "all" ]; then
+  for comp in frontend mongodb catalogue ; do
+    COMPONENT=$comp
+    CREATE_INSTANCE
+  done
+else
+  CREATE_INSTANCE
+fi
