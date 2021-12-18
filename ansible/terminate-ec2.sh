94f94092088a67ba4e0cc3bb38bd5a65798d087e
mohan <mohanreddy0532@gmail.com>
2021-12-18 18:53:27 +0530

    ec2 instance terminate

--- ansible/terminate-ec2.sh
+++ ansible/terminate-ec2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ids=aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].InstanceId| sed 's/"//g'
+ids=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].InstanceId| sed 's/"//g')
 aws ec2 terminate-instances --instance-ids ids
