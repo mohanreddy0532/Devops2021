ec94e407dcfe5ef2702dcd6c30e9e6775730beb6
mohan <mohanreddy0532@gmail.com>
2021-12-26 13:49:59 +0530

    terminate instance cli

--- ansible/terminate-ec2.sh
+++ ansible/terminate-ec2.sh
@@ -10,8 +10,8 @@ IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONE
 
   # Update the DNS record
   sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record_delete.json >/tmp/record_delete.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record_delete.json | jq
-
+  aws route53 change-resource-record-sets --change-batch file:///tmp/record_delete.json | jq
+  #removed zone id
   sleep 20
 
   #aws ec2 terminate-instances --instance-ids $ids
